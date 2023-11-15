3e428f28fa7fd5b34c5f3fcc7d7956f2e715de00
SpacebornYu <120189548+SpacebornYu@users.noreply.github.com>
2023-11-15 10:50:40 +0100

    Initial Commit

---
+++ Shortcut/ShortcutApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShortcutApp.swift
+//  Shortcut
+//
+//  Created by Yuri Mario Gianoli on 15/11/23.
+//
+
+import SwiftUI
+
+@main
+struct ShortcutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
