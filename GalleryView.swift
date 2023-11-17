9340fc80ce94c23e3fdb1d13bd9b9dd9f1d8a6d4
SpacebornYu <120189548+SpacebornYu@users.noreply.github.com>
2023-11-17 21:55:16 +0100

    updateds
    1) improved horizontal scroll views
    2) added more horizontal scroll views
    3) fixed some potitioning problems
    4) added the scroll view's title
    5) more accurate sizes for the shortcuts

--- GalleryView.swift
+++ GalleryView.swift
@@ -15,9 +15,9 @@ struct GalleryView: View {
                 .navigationTitle("Gallery")
                 .searchable(text: $searchBar, prompt: "Search")
             
-                
-                ScrollView(.vertical, showsIndicators: false){
-                    VStack (alignment: .leading) {
+            
+            ScrollView(.vertical, showsIndicators: false){
+                VStack (alignment: .leading) {
                     Text("Shortcuts for Accessibility")
                         .bold()
                         .font(.system(size: 25))
@@ -25,9 +25,9 @@ struct GalleryView: View {
                         .padding(.bottom, -25)
                     ScrollView (.horizontal, showsIndicators: false){
                         HStack(spacing: 25) {
-                            ForEach(0..<10) {
+                            ForEach(1..<11) {
                                 
-                                Text("pippo \($0)")
+                                Text("cristo iddio \($0)")
                                     .foregroundStyle(.white)
                                     .font(.title)
                                     .frame(width: 350, height: 140)
@@ -39,19 +39,20 @@ struct GalleryView: View {
                     }
                     .padding(.leading, 10)
                     .frame(height: 200)
-                        Text("Get stuff Done")
-                            .bold()
-                            .font(.system(size: 25))
-                            .padding(.leading, 20)
-                            .padding(.bottom, 10)
+                    Text("Get stuff Done")
+                        .bold()
+                        .font(.system(size: 25))
+                        .padding(.leading, 20)
+                        .padding(.bottom, 30)
                     ScrollView (.horizontal, showsIndicators: false){
-                        HStack(spacing: 25) {
-                            ForEach(0..<10) {
+                        HStack(spacing: 15) {
+                            ForEach(1..<11) {
                                 
-                                Text("pippo \($0)")
+                                Text("Trasforma il testo in audio \($0)")
+                                    .font(.system(size: 20))
                                     .foregroundStyle(.white)
                                     .font(.title)
-                                    .frame(width: 175, height: 120)
+                                    .frame(width: 165, height: 110)
                                     .background(Color.green)
                                     .cornerRadius(25.0)
                             }
@@ -59,14 +60,22 @@ struct GalleryView: View {
                         
                     }
                     .padding(.leading, 10)
+                    .padding(.bottom, 10)
+                    
+                    
+                    Text("Quick Shortcuts")
+                        .bold()
+                        .font(.system(size: 25))
+                        .padding(.leading, 20)
+                        .padding(.bottom, -20)
                     ScrollView (.horizontal, showsIndicators: false){
-                        HStack(spacing: 25) {
-                            ForEach(0..<10) {
+                        HStack(spacing: 15) {
+                            ForEach(1..<11) {
                                 
-                                Text("pippo \($0)")
+                                Text("pippo Franco \($0)")
                                     .foregroundStyle(.white)
                                     .font(.title)
-                                    .frame(width: 175, height: 120)
+                                    .frame(width: 165, height: 110)
                                     .background(Color.blue)
                                     .cornerRadius(25.0)
                             }
@@ -75,7 +84,71 @@ struct GalleryView: View {
                     }
                     .padding(.leading, 10)
                     .frame(height: 200)
+                    Text("Essentials")
+                        .bold()
+                        .font(.system(size: 25))
+                        .padding(.leading, 20)
+                        .padding(.top, -30)
+                    ScrollView (.horizontal, showsIndicators: false){
+                        HStack(spacing: 15) {
+                            ForEach(1..<11) {
+                                
+                                Text("pippo: no \($0)")
+                                    .foregroundStyle(.white)
+                                    .font(.title)
+                                    .frame(width: 165, height: 110)
+                                    .background(Color.purple)
+                                    .cornerRadius(25.0)
+
+                            }
+                        }
+                        
+                    }
+                    .padding(.leading, 10)
+                    .frame(height: 150)
+                    Text("Collaborate Better")
+                        .bold()
+                        .font(.system(size: 25))
+                        .padding(.leading, 20)
+                        .padding(.bottom, -20)
+                    ScrollView (.horizontal, showsIndicators: false){
+                        HStack(spacing: 15) {
+                            ForEach(1..<11) {
+                                
+                                Text("pippo: si \($0)")
+                                    .foregroundStyle(.white)
+                                    .font(.title)
+                                    .frame(width: 165, height: 110)
+                                    .background(Color.orange)
+                                    .cornerRadius(25.0)
+                            }
+                        }
+                        
+                    }
+                    .padding(.leading, 10)
+                    .frame(height: 200)
                     
+                    Text("Photography")
+                        .bold()
+                        .font(.system(size: 25))
+                        .padding(.leading, 20)
+                        .padding(.top, -30)
+                    ScrollView (.horizontal, showsIndicators: false){
+                        HStack(spacing: 15) {
+                            ForEach(1..<11) {
+                                
+                                Text("pippo Baudo \($0)")
+                                    .foregroundStyle(.white)
+                                    .font(.title)
+                                    .frame(width: 165, height: 110)
+                                    .background(Color.black)
+                                    .cornerRadius(25.0)
+                            }
+                        }
+                        
+                    }
+                    .padding(.leading, 10)
+                    .frame(height: 150)
                 }
             }
         }
